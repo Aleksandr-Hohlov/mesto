7ee003fc31eaf51203bc7949c0fcbcb20c915ac3
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-04-12 12:07:17 +0300

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,22 @@
 const initialCards = [
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    name: 'Река Алибек',
+    link: 'https://i.postimg.cc/3x1kZVYJ/Alibek-River.jpg'
+  },{
+    name: 'Гебиусские водопады',
+    link: 'https://i.postimg.cc/MZRZZNc3/Gebius-Waterfalls.jpg'
+  },{
+    name: 'Скала Киселева',
+    link: 'https://i.postimg.cc/L63HLP3z/Kiselyov-rock.jpg'
+  },{
+    name: 'Домбай',
+    link: 'https://i.postimg.cc/MK6FvfPM/Dombaya-Mountains.jpg'
+  },{
+    name: 'Черное море',
+    link: 'https://i.postimg.cc/VL8fJgnf/The-Black-Sea.jpg'
+  },{
+    name: 'Гора Ключевая',
+    link: 'https://i.postimg.cc/mgdB6Gqk/Klychevay.jpg'
   }
 ];
 
