d42b241e1c461210970c3cb97baeaf4981be914a
Александр Хохлов <94252054+Aleksandr-Hohlov@users.noreply.github.com>
2022-08-14 21:00:06 +0300

    Add files via upload

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,29 +1,11 @@
 export const buttonEditProfile = document.querySelector('.profile__edit-button');
 export const buttonEditAvatar = document.querySelector('.profile__avatar-edit');
-export const editProfileCloseButton = document.querySelector('.popup__close-button_edit-profile');
-export const popupEditProfile = document.querySelector('#popup__edit-profile');
-export const formEditProfile = document.querySelector('.popup__container');
-export const nameProfile = document.querySelector('.profile__title');
-export const jobProfile = document.querySelector('.profile__subtitle');
 export const addButton = document.querySelector('.profile__add-button');
 
 export const nameSelector = '.profile__title';
 export const aboutSelector = '.profile__subtitle';
 export const avatarSelector = '.profile__avatar';
-
-export const popupNewPlace = document.querySelector('#popup__new-place');
-export const formNewPlace = document.querySelector('.popup__form_new-place');
-export const newPlaceCloseButton = document.querySelector('.popup__close-button_new-place');
-export const popupPreviewCloseButton = document.querySelector('.popup__close-button_preview');
-//export const nameInput = document.querySelector('.popup__input_type_name');
-//export const jobInput = document.querySelector('.popup__input_type_job');
-export const placeInput = document.querySelector('.popup__input_type_name-place');
-export const linkInput = document.querySelector('.popup__input_type_link-place');
 export const areaElements = '.elements';
-
-export const popupPreviewImg = document.querySelector('#popup__img');
-export const previewImg = document.querySelector('.popup__preview');
-export const popupSubtitle = document.querySelector('.popup__subtitle');
 
 export const editForm = document.querySelector('.popup__form_edit-profile');
 export const addCardForm = document.querySelector('.popup__form_new-place');
